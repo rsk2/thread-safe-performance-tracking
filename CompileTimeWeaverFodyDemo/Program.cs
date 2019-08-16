690261e8512b960dc93acecdf6939fee5b6e855c
Rohan <rsureshkumar@empyra.com>
2019-08-16 12:52:02 +0530

    Committing files

---
+++ CompileTimeWeaverFodyDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileTimeWeaverFodyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCompileTimeWeaverFody();
+            Console.ReadLine();
+        }
+
+        [TimeIt]
+        private static void TestCompileTimeWeaverFody()
+        {
+            Console.WriteLine("Sleeping for 5 seconds");
+            System.Threading.Thread.Sleep(5000);
+            try
+            {
+                throw new NullReferenceException();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception message: "+ ex.Message);
+            }
+        }
+    }
+}
---
+++ CompileTimeWeaverFodyDemo/TimeIt.cs
@@ -0,0 +1,64 @@
+﻿using CompileTimeWeaver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileTimeWeaverFodyDemo
+{
+    public class TimeItAttribute : AdviceAttribute
+    {
+        public override object Advise(IInvocation invocation)
+        {
+            // do something before target method is Called
+            // ...
+            Console.WriteLine("Entering " + invocation.Method.Name);
+
+            try
+            {
+                return invocation.Proceed();    // call the next advice in the "chain" of advice pipeline, or call target method
+            }
+            catch (Exception e)
+            {
+                // do something when target method throws exception
+                // ...
+                Console.WriteLine("TimeIt catches an exception: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                // do something after target method is Called
+                // ...
+                Console.WriteLine("Leaving " + invocation.Method.Name);
+            }
+        }
+
+        public override async Task<object> AdviseAsync(IInvocation invocation)
+        {
+            // do something before target method is Called
+            // ...
+            Console.WriteLine("Entering async " + invocation.Method.Name);
+
+            try
+            {
+                return await invocation.ProceedAsync().ConfigureAwait(false);   // asynchroniously call the next advice in advice pipeline, or call target method
+            }
+            catch (Exception e)
+            {
+                // do something when target method throws exception
+                // ...
+                Console.WriteLine("MyAdvice catches an exception: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                // do something after target method is Called
+                // ...
+                Console.WriteLine("Leaving async " + invocation.Method.Name);
+            }
+        }
+    }
+
+   
+}
